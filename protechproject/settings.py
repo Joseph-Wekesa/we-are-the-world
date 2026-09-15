0071fd573701a0fb0277173bb0567d7c03b12e0c
unknown <reboprinxe7@gmail.com>
2026-09-16 00:36:35 +0300

    Use environment secret key

--- protechproject/settings.py
+++ protechproject/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y-id=j9%t5d(%*a#@#klaawh$fi=v72p6$z2_@p753453+_h8$'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
